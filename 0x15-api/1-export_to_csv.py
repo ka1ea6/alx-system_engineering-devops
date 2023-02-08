9d5e791244dd60916f7397e2e10df232680684bb
ka1ea6 <kaleab77gulilat@gmail.com>
2023-02-08 12:05:21 +0300

    1 writing to csv

--- 0x15-api/1-export_to_csv.py
+++ 0x15-api/1-export_to_csv.py
@@ -4,23 +4,27 @@ returning an employees TODO list progress'''
 
 import sys
 import requests
-import json
+import csv
+
+
 if len(sys.argv) > 1:
     employee_id = int(sys.argv[1])
+    csv_headers = ['USER_ID', 'USERNAME',
+                   'TASK_COMPLETED_STATUS', 'TASK_TITLE']
     user = requests.get(
         f"https://jsonplaceholder.typicode.com/users/{employee_id}")
     todos = requests.get(
         f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos/")
 
-    done_tasks = 0
-    done_tasks_str = ""
+    username = user.json()['username']
+    rows = []
     for todo in todos.json():
-        if todo['completed']:
-            done_tasks += 1
-            done_tasks_str += f"\t {todo['title']}\n"
-    print(
-        f"Employee {user.json()['name']} is done with tasks ({done_tasks}/{ len(todos.json())}):")
-    print(done_tasks_str[:-2])
+        rows.append({'USER_ID': employee_id, 'USERNAME': username,
+                    'TASK_COMPLETED_STATUS': todo['completed'], 'TASK_TITLE': todo['title']})
+
+    with open(f"{employee_id}.csv", 'w') as f:
+        writer = csv.DictWriter(f, fieldnames=csv_headers)
+        writer.writerows(rows)
 
 else:
     print("Usage: ./0-gather_data_from_an_API.py <employee_id>")
